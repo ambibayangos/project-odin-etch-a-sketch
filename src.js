24f496d0469071dd80c46f601c19065b295b9199
Arturo Bayangos <ambibayangos@gmail.com>
2023-04-26 23:18:42 +1200

    Make square grid from divs
    I also added some styles to make it look like an actual grid on the
    page.

---
+++ src.js
@@ -0,0 +1,18 @@
+const gridBoxElement = document.querySelector('.grid-box');
+
+function createGrid(gridSize){
+    for(let row = 0; row < gridSize ; row++){
+        let rowElement = document.createElement('div');
+        rowElement.setAttribute('id',`row${row}`);
+        rowElement.classList.add('row');
+            for(let col = 0; col < gridSize; col++){
+                let colElement = document.createElement('div');
+                colElement.setAttribute('id', `row${row}-col${col}`);
+                colElement.classList.add('col');
+                rowElement.append(colElement);
+            }
+            gridBoxElement.append(rowElement);
+        }
+}
+
+createGrid(10);
